c78c510449b972ff706caa0a867db24e2eeb0a4c
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2021-07-11 18:37:48 +0200

    Initial commit

---
+++ FirstPersonMod/FirstPersonMod.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace FirstPersonMod
+{
+    [BepInPlugin("de.kaleidox.dsp.fpm" + Strings.Channel, Strings.Name, Strings.Version)]
+    public class FirstPersonMod : BaseUnityPlugin
+    {
+        private Harmony _harmony;
+        
+        private void Awake()
+        {
+            _harmony = new Harmony(typeof(FirstPersonMod).FullName);
+        }
+
+        private void Start()
+        {
+            _harmony.PatchAll(typeof(FirstPersonMod));
+            FirstPersonDebug.Log("Started!");
+        }
+
+        [HarmonyPatch(typeof(EasyCamera), "UserInput"), HarmonyPostfix]
+        private void CameraUserInputPostfix(EasyCamera __instance)
+        {
+            __instance.distance = -10;
+        }
+
+        private void Reset()
+        {
+            _harmony.UnpatchAll();
+        }
+    }
+
+    public static class FirstPersonDebug
+    {
+        private static readonly bool debug = true;
+        internal static bool verbose = false;
+        public static ILogger log => Debug.unityLogger;
+
+        internal static void Log(string tagname, object message, Exception exception = null)
+        {
+            log?.Log($"[{Strings.LogPrefix} - {tagname}]", message + (exception == null
+                ? string.Empty
+                : "\nSource: " + exception.Source + "\nException:\n" + exception));
+        }
+
+        public static void Log(object message, Exception exception = null)
+        {
+            Log("Info", message, exception);
+        }
+
+        public static void LogDebug(object message, Exception exception = null)
+        {
+            if (!debug)
+                return;
+            Log("Debug", message, exception);
+        }
+
+        public static void LogVerbose(object message, Exception exception = null)
+        {
+            if (!verbose)
+                return;
+            Log("Verbose", message, exception);
+        }
+    }
+
+    public static class Strings
+    {
+        public const string Channel = "Alpha";
+        public const string Name = "OrbitalMiner" + Channel;
+        public const string Version = "2.4.0" + Channel;
+        public const string LogPrefix = "[" + Name + "]";
+    }
+}
