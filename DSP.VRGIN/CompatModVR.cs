5d538fef9c7140ce708f08b0d1a9788016b70bdc
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2021-09-26 12:32:05 +0200

    DSP.VRGIN

---
+++ DSP.VRGIN/CompatModVR.cs
@@ -0,0 +1,11 @@
+﻿using BepInEx;
+
+namespace DSP.VRGIN
+{
+    [BepInPlugin("DSP.VIRGIN", "VR Support for DSP using VRGIN", Version)]
+    [BepInDependency("BepInEx.VRGIN")]
+    public class CompatModVR : BaseUnityPlugin
+    {
+        public const string Version = "0.0.1";
+    }
+}
---
+++ DSP.VRGIN/VRginContext.cs
@@ -0,0 +1,10 @@
+﻿using BepInEx;
+
+namespace DSP.VRGIN
+{
+    [BepInPlugin("BepInEx.VRGIN.Context", "DSP Context Mappings for VRGIN", Version)]
+    public class VRginContext
+    {
+        public const string Version = "0.0.1";
+    }
+}
