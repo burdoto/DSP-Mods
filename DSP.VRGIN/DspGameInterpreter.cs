8b61eba35396b535f826662cce7f8243be08e6aa
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2021-09-26 13:36:06 +0200

    basic gameinterpreter

---
+++ DSP.VRGIN/DspGameInterpreter.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using VRGIN.Core;
+
+namespace DSP.VRGIN
+{
+    public class DspGameInterpreter : GameInterpreter
+    {
+        public override Camera FindCamera() => GameCamera.main;
+
+        public override bool IsIrrelevantCamera(Camera blueprint) => blueprint != GameCamera.main;
+    }
+}
--- DSP.VRGIN/DspVrContextProvider.cs
+++ DSP.VRGIN/DspVrContextProvider.cs
@@ -3,6 +3,7 @@ using BepInEx;
 using BepInEx.VRGIN;
 using BepInEx.VRGIN.Core;
 using UnityEngine;
+using VRGIN.Controls.Speech;
 using VRGIN.Core;
 using VRGIN.Visuals;
 
@@ -14,28 +15,29 @@ namespace DSP.VRGIN
         private void Awake()
         {
             VRCore.VrContextType = typeof(DspVrContext);
+            VRCore.GameInterpreterFactory = ctx => VRManager.Create<DspGameInterpreter>(ctx);
         }
     }
 
     public sealed class DspVrContext : IVRManagerContext
     {
-        public string GuiLayer { get; }
-        public string UILayer { get; }
-        public int UILayerMask { get; }
-        public int IgnoreMask { get; }
-        public Color PrimaryColor { get; }
-        public IMaterialPalette Materials { get; }
-        public VRSettings Settings { get; }
-        public string InvisibleLayer { get; }
-        public bool SimulateCursor { get; }
-        public bool GUIAlternativeSortingMode { get; }
-        public Type VoiceCommandType { get; }
-        public float GuiNearClipPlane { get; }
-        public float GuiFarClipPlane { get; }
-        public float NearClipPlane { get; }
-        public float UnitToMeter { get; }
-        public bool EnforceDefaultGUIMaterials { get; }
-        public bool ConfineMouse { get; }
-        public GUIType PreferredGUI { get; }
+        public string GuiLayer { get; } = "UIRoot/Overlay Canvas";
+        public string UILayer { get; } = "UIRoot/Overlay Canvas/In Game";
+        public int UILayerMask { get; } = 0;
+        public int IgnoreMask { get; } = 0;
+        public Color PrimaryColor { get; } = Color.cyan;
+        public IMaterialPalette Materials { get; } = new DefaultMaterialPalette();
+        public VRSettings Settings { get; } = VRCore.LoadVrSettings();
+        public string InvisibleLayer { get; } = string.Empty;
+        public bool SimulateCursor { get; } = true;
+        public bool GUIAlternativeSortingMode { get; } = false;
+        public Type VoiceCommandType { get; } = typeof(VoiceCommand);
+        public float GuiNearClipPlane { get; } = 0;
+        public float GuiFarClipPlane { get; } = 1000;
+        public float NearClipPlane { get; } = 0;
+        public float UnitToMeter { get; } = 1;
+        public bool EnforceDefaultGUIMaterials { get; } = false;
+        public bool ConfineMouse { get; } = false;
+        public GUIType PreferredGUI { get; } = GUIType.uGUI;
     }
 }
