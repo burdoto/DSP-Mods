e9a63f0d28192ae8da1293e3d8e5f3e8c700a732
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2021-09-26 12:41:02 +0200

    add core

---
+++ DSP.VRGIN/DspVrContextProvider.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using BepInEx;
+using BepInEx.VRGIN.Core;
+using UnityEngine;
+using VRGIN.Core;
+using VRGIN.Visuals;
+
+namespace DSP.VRGIN
+{
+    [BepInPlugin("BepInEx.VRGIN.Context", "DSP Context Mappings for VRGIN", Version)]
+    public class DspVrContextProvider : BaseUnityPlugin
+    {
+        public const string Version = "0.0.1";
+
+        private void Awake()
+        {
+            VRCore.VrContextType = typeof(DspVrContext);
+        }
+    }
+
+    public sealed class DspVrContext : IVRManagerContext
+    {
+        public string GuiLayer { get; }
+        public string UILayer { get; }
+        public int UILayerMask { get; }
+        public int IgnoreMask { get; }
+        public Color PrimaryColor { get; }
+        public IMaterialPalette Materials { get; }
+        public VRSettings Settings { get; }
+        public string InvisibleLayer { get; }
+        public bool SimulateCursor { get; }
+        public bool GUIAlternativeSortingMode { get; }
+        public Type VoiceCommandType { get; }
+        public float GuiNearClipPlane { get; }
+        public float GuiFarClipPlane { get; }
+        public float NearClipPlane { get; }
+        public float UnitToMeter { get; }
+        public bool EnforceDefaultGUIMaterials { get; }
+        public bool ConfineMouse { get; }
+        public GUIType PreferredGUI { get; }
+    }
+}
